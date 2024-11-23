79523e019ea6887223541319e8a2c8cf7d0d8d64
Dobbs, Matthew <md9p8@umsystem.edu>
2024-11-22 20:18:40 -0600

    add required comments

--- graphc.py
+++ graphc.py
@@ -5,13 +5,21 @@ import plotext as plot  # external dependency; must be installed
 import numpy as np
 
 def print_graph_c(activity_logs_by_student: dict[str, list[LogEntry]] ):
-  # TODO instantiate array to sum minutes per day
-  
+  """
+  print_graph_c
+  AUTHOR:  Matthew Dobbs
+  PURPOSE: gathers graph c data from common dictionary, generates graph, and exits
+  INPUTS:  dict: k,v store comprised of log files and log entries in log files
+  OUTPUTS: none (console output only) 
+  """
+
+  # dict to store dates and minutes from log files
   minutes_by_date = {}
 
   for key in activity_logs_by_student:
     print("\n\n\n" + key + "'s Activity Log File:\n\n")
     for log_entry in activity_logs_by_student[key]:
+      # convert date, start time, and end time fields to datetime format
       date_convert = datetime.strptime(log_entry.date, '%m/%d/%Y').date().strftime('%Y%m%d')
       time1_convert = datetime.strptime(log_entry.start_time, '%H:%M')
       time2_convert = datetime.strptime(log_entry.end_time, '%H:%M')
@@ -19,18 +27,18 @@ def print_graph_c(activity_logs_by_student: dict[str, list[LogEntry]] ):
       # print('START: ', time1_convert.time(), ' END: ', time2_convert.time())
       delta = time2_convert - time1_convert
 
+      # if date exists in dict, add minutes logged to existing dict.
+      # else, create dict and set value equal to minutes logged in current entry
       if date_convert in minutes_by_date:
         minutes_by_date[date_convert] += int(delta.seconds/60)
       else:
         minutes_by_date[date_convert] = int(delta.seconds/60)
-      # print(minutes_by_date[date_convert])
-      # print('DELTA: ', delta)
-      # print(date_convert)
 
+
+  # convert generated dict to sorted lists for graph
   list_to_sort = list(minutes_by_date.keys())
   list_to_sort.sort()
   sorted_minutes_by_date = {i: minutes_by_date[i] for i in list_to_sort}
-  # print(sorted_minutes_by_date)
   dates = list(sorted_minutes_by_date.keys())
   minutes = list(sorted_minutes_by_date.values())
 
@@ -38,8 +46,8 @@ def print_graph_c(activity_logs_by_student: dict[str, list[LogEntry]] ):
   plot.title("Graph C - Total Team Minutes by Date")
   plot.xlabel('Date')
   plot.ylabel('Team Minutes')
-  # whole_x_ticks = np.arange(int(min(dates)), int(max(dates)) + 1)
-  # plot.xticks(whole_x_ticks)
+  # Reduce plot size to fit terminal window with continue msg
+  plot.plot_size(plot.terminal_width(), plot.terminal_height()-2)
 
   # Set y ticks to whole numbers (if needed)
   whole_y_ticks = np.arange(int(min(minutes)), int(max(minutes)))
